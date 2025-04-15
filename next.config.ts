43aa0abaaf0f5d1a23a3d34471264ad8c01905a9
coder-HDY <d_huang@dreamarts.co.jp>
2025-04-15 20:53:53 +0800

    feat: init

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 忽略构建时的 ESLint 报错
+  },
 };
 
 export default nextConfig;
